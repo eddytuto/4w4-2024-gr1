2dd9fa3aa87fcd5d8c1c221c7ab5190d0ed62091
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-03-06 08:56:38 -0500

    s5c1 solution du labo3

--- index.php
+++ index.php
@@ -40,10 +40,21 @@
                 }
             */
             if (have_posts()):
-                while(have_posts()): the_post(); ?>
+
+                while(have_posts()): the_post();
+                 $titre = get_the_title();
+                 $sigle = substr($titre,0,7);
+                 $pos_parenthese = strpos($titre, '(');
+                 $duree = substr($titre,$pos_parenthese+1,-1);
+                 $titre = substr($titre,7, $pos_parenthese-7);
+                
+                
+                ?>
                 <div class="carte">
-                    <h3><?php the_title(); ?></h3>  
-                    <p><?php echo wp_trim_words(get_the_content(),20); ?> </p>
+                    <h4><?php echo $sigle; ?></h4>
+                    <h3><?php echo $titre; ?></h3>  
+                    <p><?php echo wp_trim_words(get_the_content(),10); ?> </p>
+                    <h4>Durée: <?php echo $duree; ?></h4>
                 </div> 
                <?php endwhile; ?>
             <?php endif; ?>
