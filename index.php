bf0a8c0f0f1de96295c1aa85f3cfd120ebe91036
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-03-07 11:05:20 -0500

    s5c2 header.php, template-part et menu adaptatif

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thème du groupe #1</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/normalize.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-</head>
-<body>
-    <nav id="menu" class="global">
-        <header class="menu__header">
-            <?php wp_nav_menu(array("container" => "nav")); ?>
-        </header>
-</nav>
+
+    <?php get_header(); ?>
     <div id="entete" class="global">
         <section class="entete__header">
             <h1>Thème du groupe #1 (h1)</h1>
@@ -23,11 +7,7 @@
             <h3>TIM-Collège de Maisonneuve</h3>
             <button>Événements</button>
         </section>
-        <div class="vague">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: var(--couleur-arriere-accueil);"></path>
-            </svg>
-        </div>
+    <?php get_template_part("gabarits/vague"); ?>
     </div>
     <div id="accueil" class="global">
         <section>
@@ -75,11 +55,7 @@
         <section>
             <h2>Galerie (h2)</h2>
         </section>
-        <div class="vague">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: var(--couleur-arriere-footer);"></path>
-            </svg>
-        </div>
+  <?php  get_template_part('gabarits/vague'); ?>
     </div>
     <div id="footer" class="global">
 
