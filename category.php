39d846ec03dd4dbfc9b2f9e9ec725d945ab61a8c
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-03-20 11:50:34 -0400

    s7c1 ajout de category.php, the_category et pre_get_post dans functions.php

--- category.php
+++ category.php
@@ -1,17 +1,9 @@
 
     <?php get_header(); ?>
-    <div id="entete" class="global">
-        <section class="entete__header">
-            <h1><?php echo get_bloginfo("name") ?></h1>
-            <h2><?php echo get_bloginfo("description") ?></h2>
-            <h3>TIM-Collège de Maisonneuve</h3>
-            <button>Événements</button>
-        </section>
-    <?php get_template_part("gabarits/vague"); ?>
-    </div>
+    
     <div id="accueil" class="global">
         <section>
-            <h2>Accueil (h2)</h2>
+            <h2>Destination par catégorie</h2>
             <div class="destinations">
             <?php if (have_posts()):
                 while(have_posts()): the_post(); ?>
@@ -27,11 +19,7 @@
             </div>
         </section>
     </div>
-    <div id="evenement" class="global diagonal">
-        <section>
-            <h2>Événement (h2)</h2>
-        </section>
-    </div>
+
     <div id="galerie" class="global">
         <section>
             <h2>Galerie (h2)</h2>
