cf411a35331d8568c24e7a00cf82953afa9d2a6f
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-05-16 12:10:24 -0400

    s14c2 ajout de deux template-part gabarit: carte et galerie

--- front-page.php
+++ front-page.php
@@ -16,21 +16,17 @@
     </div>
     <div id="accueil" class="global">
         <section>
-            <h2>Accueil (h2)</h2>
+            <h2>Destinations populaires</h2>
             <div class="destinations">
             <?php if (have_posts()):
                 while(have_posts()): the_post(); ?>
-                <div class="carte">
+                <?php
+                $ma_carte = "carte"; 
+                if (in_category('galerie')) {
+                    $ma_carte = "galerie";
+                }
+                get_template_part("gabarits/categorie", $ma_carte); ?>
 
-                    <?php
-                    // permet d'afficher l'image de l'article qui a été mis en avant 
-                    the_post_thumbnail('thumbnail'); ?>
-                    <h3><?php the_title(); ?></h3>  
-                    <p><?php echo wp_trim_words(get_the_content(),10); ?> </p>
-                    <?php the_category(); ?>
-                      <!-- ajoute un lien vers l'article   -->
-                    <a href="<?php  the_permalink(); ?>">Suite</a>
-                </div> 
                <?php endwhile; ?>
             <?php endif; ?>
             </div>
